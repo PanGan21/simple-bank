4377c6f51b7ae2fd7acce334a4ab66e70e7e2e05
PanGan21 <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-12-01 23:02:48 +0100

    setup server and api

---
+++ api/server.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	db "github.com/PanGan21/simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for the service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs HTTP server on a specific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+// error serializer to json
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
